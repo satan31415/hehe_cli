ced12914ca927e05b01cdc287ff5d1cdf30714b4
471867900@qq.com <471867900@qq.com>
2024-02-22 18:03:35 +0800

    init

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // 可选择的框架
-  framwork:['express', 'koa', 'egg'],
+  framwork:['admin 管理后台', 'nuxt 官网页面', 'uni 小程序'],
   // 框架对应的下载地址
   foramworkUrl:{
-    express:'git@gitee.com:beiyaoyaoyao/express-template.git',
-    koa:'git@gitee.com:beiyaoyaoyao/koa-template.git',
-    egg:'git@gitee.com:beiyaoyaoyao/egg-template.git'
+    'admin 管理后台':'https://gitee.com/satan31415/heh_cli.git',
+    'nuxt 官网页面':'https://gitee.com/satan31415/heh_cli.git',
+    'uni 小程序':'https://github.com/satan31415/hehe_cli.git'
   }
 }
